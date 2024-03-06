d4f7cc48f288f6d2117c5b53bdd7d399e0d2ece1
2403110272 <162427678+2403110272@users.noreply.github.com>
2024-03-06 18:03:02 +0900

    Add files via upload

---
+++ Print01.java
@@ -0,0 +1,18 @@
+package sec04;
+
+public class Print01 {
+
+	public static void main(String[] args) {
+		String myName = "박서현";
+		int myAge=24;
+		
+		System.out.println("이름:" + myName);
+		System.out.println("나이:" + myAge);
+		System.out.printf("  이름:  " + myName +  "  나이:  " + myAge );
+		
+	//	System.out.println("이름;"+myName);
+	//	System.out.println("이름;"+myName);
+		
+	}
+
+}
