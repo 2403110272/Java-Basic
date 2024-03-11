4d7ccf04c3df0b865dd835edde3bc0660a99da9c
2403110272 <162427678+2403110272@users.noreply.github.com>
2024-03-11 18:12:54 +0900

    Add files via upload

---
+++ Scanner01.java
@@ -0,0 +1,16 @@
+package scanner;
+
+public class Scanner01 {
+
+	public static void main(String[] args) {
+		int x = 10;
+		double y = 2.5;
+		
+		System.out.println(x += y);
+		System.out.println(x -= y);
+		System.out.println(x *= y);
+		System.out.println(x /= y);
+
+	}
+
+}
---
+++ Scanner02.java
@@ -0,0 +1,14 @@
+package scanner;
+
+public class Scanner02 {
+
+	public static void main(String[] args) {
+	final double PI = 3.14;
+	int r;
+	r = 10;
+	System.out.println(PI * r * r);
+			
+
+	}
+
+}
